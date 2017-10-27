fe087fa7f90c31ef3bdcbdadded120d3f326b42e
Shashank Vuppala <svuppala@microsoft.com>
2017-10-27 06:20:09 -0700

    Add project files.

---
+++ Practice/Arrays_Strings/DecodeWays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class GroupAnagramsProgram
+    {
+        static void Main(string[] args)
+        {
+            string message = "1234";
+
+            Dictionary<string, string> keyValue = new Dictionary<string, string>();
+            keyValue.Add("1", "A");
+            keyValue.Add("2", "B");
+            keyValue.Add("3", "C");
+            keyValue.Add("4", "D");
+            keyValue.Add("5", "E");
+            keyValue.Add("6", "F");
+            keyValue.Add("7", "G");
+            keyValue.Add("8", "H");
+            keyValue.Add("9", "I");
+            keyValue.Add("10", "J");
+            keyValue.Add("11", "K");
+            keyValue.Add("12", "L");
+            keyValue.Add("13", "M");
+            keyValue.Add("14", "N");
+            keyValue.Add("15", "O");
+            keyValue.Add("16", "P");
+            keyValue.Add("17", "Q");
+            keyValue.Add("18", "R");
+            keyValue.Add("19", "S");
+            keyValue.Add("20", "T");
+            keyValue.Add("21", "U");
+            keyValue.Add("22", "V");
+            keyValue.Add("23", "W");
+            keyValue.Add("24", "X");
+            keyValue.Add("25", "Y");
+            keyValue.Add("26", "Z");
+
+            char[] msgChars = message.ToCharArray();
+
+            for()
+
+        }
+    }
+}
---
+++ Practice/Arrays_Strings/GroupAnagramsProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class GroupAnagramsProgram
+    {
+        static void Main(string[] args)
+        {
+            string[] strList = new string[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+            Dictionary<string, List<string>> Results = GroupAnagrams(strList);
+            Console.Write(Results);
+        }
+
+        public static Dictionary<string, List<string>> GroupAnagrams(string[] inputList)
+        {
+            Dictionary<string, List<string>> ht = new Dictionary<string, List<string>>();
+
+            foreach (string str in inputList)
+            {
+                Char[] ca = str.ToCharArray(0, str.Length);
+
+                Array.Sort(ca);
+                string sortedString = new string(ca);
+
+                if (!ht.ContainsKey(sortedString))
+                {
+                    ht.Add(sortedString, new List<string>());
+                }
+
+                ht[sortedString].Add(str);
+            }
+
+            return ht;
+        }
+    }
+}
